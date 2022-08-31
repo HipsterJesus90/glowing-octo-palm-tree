4803d3b89706057d8bd8ae51e2053e2b5341d8ee
Giuliano Andrighetti Paizani <giuliano.paizani@MacBook-Pro-Giuliano.local>
2022-08-31 15:04:57 +0200

    adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao Banco de Dados")
+// mais um console log de testes
+console.log("blablabla")
+console.log("Enviando para o GITHUB")
